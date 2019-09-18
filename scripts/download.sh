6bda0d3468cd451cd0ff72f78d01ef299d479d6f
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2019-09-18 11:57:24 -0400

    Add download script and md5sums

---
+++ scripts/download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script is meant to be used with the command 'datalad run'
+
+git-annex addurl --raw --file personachat.tgz http://parl.ai/downloads/personachat/personachat.tgz
+
